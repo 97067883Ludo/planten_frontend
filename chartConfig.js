be3509f1c904dacaad9b003cf298fd6d2bc2dd91
ludotielbeke <ludotielbeke@gmail.com>
2023-05-08 10:55:51 +0200

    tailwind

---
+++ chartConfig.js
@@ -0,0 +1,17 @@
+export const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'sdf', 'much'],
+    datasets: [
+        {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40, 39, 10, 40, 39, 80, 40, 70, 1100],
+            tension: 0.4
+        }
+    ]
+}
+
+export const options = {
+    maintainAspectRatio: false,
+    pointRadius: 10,
+    pointHoverRadius: 15
+}
---
+++ components/LineChart.vue
@@ -0,0 +1,34 @@
+<template>
+    <client-only>
+        <Line :data="data" :options="options" />
+    </client-only>
+</template>
+
+<script lang="ts" setup>
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
+import * as chartConfig from '../chartConfig.js'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+let data = chartConfig.data;
+let options = chartConfig.options;
+
+</script>
